77e0e6095ce5c4e6956d1a5928a5e8ca77a223b6
StefanoPietropaolo <spietropaolo@udesa.edu.ar>
2024-11-14 18:11:17 -0300

    esquemas screens newpost,profile,home

---
+++ src/screens/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+export default Home;
---
+++ src/screens/NewPost.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+class NewPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+export default NewPost;
