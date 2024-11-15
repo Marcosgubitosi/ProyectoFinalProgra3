d68246925ee6b6f038b8a9bde7cd68b6605cae76
StefanoPietropaolo <spietropaolo@udesa.edu.ar>
2024-11-15 14:31:59 -0300

    pagina perfil del usuario

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -12,19 +12,65 @@ import { auth, db } from "../firebase/Config";
 class Profile extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = { 
+      usuarios: [],
+      posteos: [],
+      loading: true
 
     };
+  }
+  componentDidMount() {
+    db.collection('users').onSnapshot(
+        docs =>{
+                let posts = [];
+           docs.forEach( doc => {
+                posts.push({
+                    id: doc.id,
+                    data: doc.data()
+        })
+               this.setState({
+                usuarios: posts,
+                loading: false
+           })
+        })})
+    db.collection('posts').onSnapshot(
+        docs =>{
+                let posts = [];
+           docs.forEach( doc => {
+                posts.push({
+                    id: doc.id,
+                    data: doc.data()
+        })
+               this.setState({
+                posteos: posts,
+                loading: false
+           })
+        })})
   }
 
 
 
   render() {
-    const uEmail = auth.currentUser
-    console.log(uEmail);
+    const uEmail = auth.currentUser.email
+    const cantidadPosteos = this.state.posteos.filter(
+      posteo => posteo.data.email === uEmail).length;
     
     return (
       <View>
+        <Text> Mi perfil </Text>
+        <FlatList
+          data={this.state.usuarios}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => item.data.email === auth.currentUser.email ? <Text>nombre:{item.data.usuario}</Text> : null }
+        />
+        <Text>Email: {auth.currentUser.email}</Text>
+        <Text>Cantidad de posteos: {cantidadPosteos}</Text>
+        <FlatList
+          data={this.state.posteos}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => 
+          item.data.email === auth.currentUser.email ? <Text>Posteo: {item.data.message}</Text>  : null }
+        />
         
       </View>
     );
